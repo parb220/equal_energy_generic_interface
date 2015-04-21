9eec27a5d547629d4eabe8f1257e1c678bb10b58
parb220 <--global>
2015-04-21 14:13:56 -0400

    GetWeightedVariance and AggregateScaleMatrix created from the previous segmented statements

--- TaskSchedulingCPP/DispatchTuneSimulation.cpp
+++ TaskSchedulingCPP/DispatchTuneSimulation.cpp
@@ -23,6 +23,8 @@
 using namespace std; 
 
 vector<string> glob(const string &pattern);
+void GetWeightedVarianceMatrix(CEquiEnergyModel &model, int stage); 
+void AggregateScaleMatrix(CEquiEnergyModel &model, int stage); 
 
 void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,const CSampleIDWeight &mode, size_t simulation_length, bool save_space_flag)
 {
@@ -34,10 +36,7 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 	// diagnostic statistics
 	// logMDD[i][0], constant of integration using ellipticals as proposals
 	// logMDD[i][1], constant of integration using draws from the previous stage and importance.
-	// logMDD[i][2], constant of integration using draws from the previous stage as the proposals, and logMDD[i-1][0] as the estimate for the previous stage 
-	// logMDD[i][3], constant of integration using draws from the previous stage as the proposals, and logMDD[i-1][3] as the estimate for the previous stage
-	//
-	vector<vector<double> > logMDD(model.parameter->number_energy_stage+1, vector<double>(4, 0.0)); 
+	vector<vector<double> > logMDD(model.parameter->number_energy_stage+1, vector<double>(2, 0.0)); 
 	/* vector<double> consistency(model.parameter->number_energy_stage, 0.0);
 	vector<double> average_consistency(model.parameter->number_energy_stage, 0.0); 
 	vector<double> std_consistency(model.parameter->number_energy_stage, 0.0);
@@ -62,30 +61,9 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 
 	for (int stage=model.parameter->highest_stage; stage>=model.parameter->lowest_stage; stage--)
 	{
-	  time(&rawtime);
-	  cout << "DispatchTuneSimulation() - top of loop: stage=" << stage << " " << ctime(&rawtime) << endl;
+	  	time(&rawtime);
+	  	cout << "DispatchTuneSimulation() - top of loop: stage=" << stage << " " << ctime(&rawtime) << endl;
 		/////////////////////////////////////////////////////////////////////////////////
-
-		/////////////////////////////////////////////////////////////////////////////////
-                // this was the old code - hongwei suggested the code below
-		// // Highest +1 stage 
-		// if (stage == model.parameter->number_energy_stage-1)
-		// {
-		// 	HighestPlus1Stage(nNode, nInitial, model);
-		// 	// HighestPlus1Stage_Prior(nNode, nInitial, model); // Sample from prior
-		// 	/*posterior.clear(); 
-		// 	if (!model.storage->DrawAllSample(stage+1, posterior, unstructured, data_size))
-                //         {
-                //                 cerr << "EstimateLogMDD:: error occurred when loading all samples.\n";
-                //                 abort();
-                //         }
-
-		// 	// logMDD[stage+1][1]: logMDD using elliptical for draws from prior distribution
-                //         logMDD[stage+1][0] = logMDD[stage+1][1] = logMDD[stage+1][2] = logMDD[stage+1][3] = LogMDD(posterior, model, model.parameter->t[stage+1], USE_TRUNCATED_POWER, LIKELIHOOD_HEATED);
-		// 	cout << setprecision(20) << "logMDD at stage " << stage+1 << ": " << logMDD[stage+1][0] << "\t" << logMDD[stage+1][1] << "\t" << logMDD[stage+1][2] << "\t" << logMDD[stage+1][3] << endl; */
-		// }
-
-                ////////////////////////////////////////////////////////////////////////////////////
 		// Highest + 1 stage 
 		if (stage == model.parameter->highest_stage)
 		{
@@ -97,10 +75,9 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 				cout << "DispatchTuneSimulation() - drawing from prior: stage=" << stage+1 << " temperature: " << model.parameter->t[stage+1] << " " << ctime(&rawtime) << endl;
 
 				HighestPlus1Stage_Prior(nNode, nInitial, model);   // Sample from prior
-				// HighestPlus1Stage(nNode, nInitial, model);    // Sample from prior with likelihood heated extremely
 
 				time(&rawtime);
-				cout << "DispatchTuneSimulation() - done drawing from prior: stage=" << stage << " " << ctime(&rawtime) << endl;
+				cout << "DispatchTuneSimulation() - done drawing from prior: stage=" << stage+1 << " " << ctime(&rawtime) << endl;
 
 				// compute log MDD
 				time(&rawtime);			 
@@ -109,22 +86,21 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 				// get posterior draws
 				posterior.clear(); 
 				if (!model.storage->DrawAllSample(stage+1, posterior, unstructured, data_size))
-				  {
+				{
 				    cerr << "DispatchTuneSimulation: error occurred when loading all samples.\n";
 				    abort();
-				  }
+				}
 
 				logMDD[stage+1][0] = LogMDD(posterior, model, model.parameter->t[stage+1], USE_TRUNCATED_POWER, PRIOR_ONLY);
-				// logMDD[stage+1][0] = LogMDD(posterior, model, model.parameter->t[stage+1], USE_TRUNCATED_POWER, LIKELIHOOD_HEATED);
 				logMDD[stage+1][1] = 0.0;
 
 				// open MDD file for output and write log MDD for stage + 1
 				mdd_file.open(mdd_filename.c_str());
 				if (!mdd_file.is_open())
-				  {
+				{
 				    cerr << "DispatchTuneSimulation: Error opening " << mdd_filename << endl; 
 				    abort(); 	
-				  }
+				}
 				mdd_file << stage+1 << "\t" << logMDD[stage+1][0] << "\t" << logMDD[stage+1][1] << endl; 
 				cout << setprecision(20) << "logMDD at stage " << stage+1 << ": " << logMDD[stage+1][0] << "\t" << logMDD[stage+1][1] << endl; 
 
@@ -219,49 +195,11 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 		/////////////////////////////////////////////////////////////////////////////////
 		// Send out tuning jobs
 		// Only need to tune on each slave node noce, because the results will be aggregated
-		if (stage != model.parameter->number_energy_stage)
-		{
-		  time(&rawtime);
-		  cout << "DispatchTuneSimulation() - getting weighted variance matrix: stage=" << stage << " " << ctime(&rawtime) << endl;
-			// Calculate B for AdaptiveAfterSimulation_WeightedSampling_OnePass
-			// samples
-			vector<CSampleIDWeight> samples;
-                	if (!model.storage->DrawAllSample(stage+1, samples) || samples.empty() )
-                	{
-                        	cerr << "DispatchTuneSimulation() : Error in loading samples of previous stage.\n";
-                        	abort();
-                	}
-
-			// weight
-			vector<double> log_weight = model.Reweight(samples, stage, stage+1);
-        		double log_weight_sum = log_weight[0];
-        		for (int i=1; i<(int)log_weight.size(); i++)
-                		log_weight_sum = AddLogs(log_weight_sum, log_weight[i]);
-        		vector<double> weight(log_weight.size(), 0.0);
-        		for (int i=0; i<(int)log_weight.size(); i++)
-                		weight[i] = exp(log_weight[i] - log_weight_sum);
-			
-			// block_scheme
-			convert.str(string());
-        		convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_SCHEME;
-        		string block_scheme_file_name = model.parameter->storage_dir  + convert.str();
-			std::vector<TIndex> block_scheme = ReadBlockScheme(block_scheme_file_name);
-			if (block_scheme.empty())
-		                block_scheme.push_back(TIndex(0, samples[0].data.dim-1));
-			
-			vector<TDenseMatrix>B_matrix = GetBlockMatrix_WeightedSampling(samples, weight, block_scheme); 
-
-			// Bmatrix file
-			convert.str(string());
-                        convert << model.parameter->run_id << "/" << model.parameter->run_id << BMATRIX; 
-			string bmatrix_file_name = model.parameter->storage_dir  + convert.str();
-			if (!WriteBMatrixFile(bmatrix_file_name, B_matrix))
-			{
-				cerr << "DispatchTuneSimulation() : Error in writing BMatrix file.\n"; 
-				abort(); 
-			}
-		}
-
+		time(&rawtime);
+		cout << "DispatchTuneSimulation() - getting weighted variance matrix: stage=" << stage << " " << ctime(&rawtime) << endl;
+		
+		GetWeightedVarianceMatrix(model, stage); 
+		
 		sPackage[LEVEL_INDEX] = stage; 
 		sPackage[thin_INDEX] = model.parameter->thin; 
 		sPackage[THIN_INDEX] = model.parameter->THIN; 
@@ -279,34 +217,10 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 		for (int i=1; i<nNode; i++)
 			MPI_Recv(rPackage, N_MESSAGE, MPI_DOUBLE, MPI_ANY_SOURCE, TUNE_TAG_BEFORE_SIMULATION, MPI_COMM_WORLD, &status);
 
-
+		AggregateScaleMatrix(model, stage);
 		time(&rawtime);
 		cout << "DispatchTuneSimulation() - done tuning: stage=" << stage << " " << ctime(&rawtime);
 		
-		/////////////////////////////////////////////////////////////////////////////////
-		// Aggregate the scales obtained from computing nodes
-		convert.str(string()); 
-		convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_1ST << stage << ".*";
-		string block_file_pattern = model.parameter->storage_dir + convert.str();
-	        vector<string> block_file = glob(block_file_pattern);
-		
-		convert.str(string());
-        	convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_1ST << stage;
-       	 	string block_file_name = model.parameter->storage_dir + convert.str();
-		if (!model.metropolis->AggregateBlocksAndRemoveFiles(block_file, block_file_name))
-        	{
-                	cerr << "Error in reading " << block_file_pattern << " or writing " << block_file_name << endl;
-                	abort();
-        	}
-
-		if (stage != model.parameter->number_energy_stage)
-		{
-			convert.str(string());
-                        convert << model.parameter->run_id << "/" << model.parameter->run_id << BMATRIX;
-                        string bmatrix_file_name = model.parameter->storage_dir  + convert.str();
-			remove(bmatrix_file_name.c_str()); 
-		}
-
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// simualtion
 		//cout << "Simulation at " << stage << " for " << model.parameter->simulation_length << endl; 
@@ -324,41 +238,23 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 		time(&rawtime);
 		cout << "DispatchTuneSimulation() - computing MDD: stage=" << stage << " " << ctime(&rawtime) << endl;
 
-		// if (stage == model.parameter->number_energy_stage -10)
-		// {
-		// 	posterior.clear(); 
- 		// 	if (!model.storage->DrawAllSample(stage+1, posterior, unstructured, data_size))
- 		// 	{
- 		// 		cerr << "EstimateLogMDD:: error occurred when loading all samples.\n";
- 		// 		abort();
- 		// 	}
- 
-		// 	// logMDD[stage+1][1]: logMDD using elliptical for draws from prior distribution
-		// 	logMDD[stage+1][0] = logMDD[stage+1][1] = logMDD[stage+1][2] = logMDD[stage+1][3] = LogMDD(posterior, model, model.parameter->t[stage+1], USE_TRUNCATED_POWER, LIKELIHOOD_HEATED);
-		// 	cout << setprecision(20) << "logMDD at stage " << stage+1 << ": " << logMDD[stage+1][0] << "\t" << logMDD[stage+1][1] << "\t" << logMDD[stage+1][2] << "\t" << logMDD[stage+1][3] << endl; 
-		// }
-		if (stage <= model.parameter->number_energy_stage - 1)
-		{
 
-		        // at model.parameter->highest_stage, posterior at stage+1 is loaded at top of loop, otherwise it
-		        // is loaded in the previous loop below.
-			logMDD[stage][1] = LogMDD_Importance(posterior, model, stage+1, stage, logMDD[stage+1][1], LIKELIHOOD_HEATED); 
-			//proposal = posterior; 
-			posterior.clear(); 
-        		if (!model.storage->DrawAllSample(stage, posterior, unstructured, data_size))
-        		{	
-               			cerr << "EstimateLogMDD:: error occurred when loading all samples.\n";
-               			abort();
-        		}
-	     		logMDD[stage][0] = LogMDD(posterior, model, model.parameter->t[stage], USE_TRUNCATED_POWER, LIKELIHOOD_HEATED);
-			//logMDD[stage][2] = LogMDD(proposal, posterior, model, stage+1, stage, logMDD[stage+1][0]); 
-			//logMDD[stage][3] = LogMDD(proposal, posterior, model, stage+1, stage, logMDD[stage+1][3]); 
+	        // at model.parameter->highest_stage, posterior at stage+1 is loaded at top of loop, otherwise it
+	        // is loaded in the previous loop below.
+		logMDD[stage][1] = LogMDD_Importance(posterior, model, stage+1, stage, logMDD[stage+1][1], LIKELIHOOD_HEATED); 
+		//proposal = posterior; 
+		posterior.clear(); 
+        	if (!model.storage->DrawAllSample(stage, posterior, unstructured, data_size))
+        	{	
+               		cerr << "EstimateLogMDD:: error occurred when loading all samples.\n";
+               		abort();
+        	}
+	     	logMDD[stage][0] = LogMDD(posterior, model, model.parameter->t[stage], USE_TRUNCATED_POWER, LIKELIHOOD_HEATED);
 		
-			mdd_file << stage << "\t" << logMDD[stage][0] << "\t" << logMDD[stage][1] << endl;
-			mdd_file.flush();
-			cout << setprecision(20) << "logMDD at stage " << stage << ": " << logMDD[stage][0] << "\t" << logMDD[stage][1] << endl; 
-			cout.flush();
-		}
+		mdd_file << stage << "\t" << logMDD[stage][0] << "\t" << logMDD[stage][1] << endl;
+		mdd_file.flush();
+		cout << setprecision(20) << "logMDD at stage " << stage << ": " << logMDD[stage][0] << "\t" << logMDD[stage][1] << endl; 
+		cout.flush();
 
 		/*if (stage == model.parameter->number_energy_stage-1 )
 			consistency[stage-1] = CheckConvergency(model, stage-1, stage, logMDD[stage][stage], average_consistency[stage-1], std_consistency[stage-1], LB_ESS[stage-1]);
@@ -400,4 +296,70 @@ void DispatchTuneSimulation(int nNode, int nInitial, CEquiEnergyModel &model,con
 
 	delete []sPackage; 
 	delete []rPackage; 
+}
+
+void GetWeightedVarianceMatrix(CEquiEnergyModel &model, int stage)
+{
+	// Calculate B for AdaptiveAfterSimulation_WeightedSampling_OnePass
+	// samples
+	vector<CSampleIDWeight> samples;
+        if (!model.storage->DrawAllSample(stage+1, samples) || samples.empty() )
+        {
+        	cerr << "DispatchTuneSimulation() : Error in loading samples of previous stage.\n";
+        	abort();
+        }
+
+	// weight
+	vector<double> log_weight = model.Reweight(samples, stage, stage+1);
+       	double log_weight_sum = log_weight[0];
+       	for (int i=1; i<(int)log_weight.size(); i++)
+		log_weight_sum = AddLogs(log_weight_sum, log_weight[i]);
+       	vector<double> weight(log_weight.size(), 0.0);
+       	for (int i=0; i<(int)log_weight.size(); i++)
+                weight[i] = exp(log_weight[i] - log_weight_sum);
+			
+	// block_scheme
+	stringstream convert; 
+	convert.str(string());
+        convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_SCHEME;
+        string block_scheme_file_name = model.parameter->storage_dir  + convert.str();
+	std::vector<TIndex> block_scheme = ReadBlockScheme(block_scheme_file_name);
+	if (block_scheme.empty())
+                block_scheme.push_back(TIndex(0, samples[0].data.dim-1));
+			
+	vector<TDenseMatrix>B_matrix = GetBlockMatrix_WeightedSampling(samples, weight, block_scheme); 
+
+	// Bmatrix file
+	convert.str(string());
+       	convert << model.parameter->run_id << "/" << model.parameter->run_id << BMATRIX; 
+	string bmatrix_file_name = model.parameter->storage_dir  + convert.str();
+	if (!WriteBMatrixFile(bmatrix_file_name, B_matrix))
+	{
+		cerr << "DispatchTuneSimulation() : Error in writing BMatrix file.\n"; 
+		abort(); 
+	}
+}
+
+void AggregateScaleMatrix(CEquiEnergyModel &model, int stage)
+// Aggregate the scales obtained from computing nodes
+{
+	stringstream convert; 
+	convert.str(string()); 
+	convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_1ST << stage << ".*";
+	string block_file_pattern = model.parameter->storage_dir + convert.str();
+	vector<string> block_file = glob(block_file_pattern);
+		
+	convert.str(string());
+       	convert << model.parameter->run_id << "/" << model.parameter->run_id << BLOCK_1ST << stage;
+       	string block_file_name = model.parameter->storage_dir + convert.str();
+	if (!model.metropolis->AggregateBlocksAndRemoveFiles(block_file, block_file_name))
+       	{
+               	cerr << "Error in reading " << block_file_pattern << " or writing " << block_file_name << endl;
+               	abort();
+       	}
+
+	convert.str(string());
+        convert << model.parameter->run_id << "/" << model.parameter->run_id << BMATRIX;
+        string bmatrix_file_name = model.parameter->storage_dir  + convert.str();
+	remove(bmatrix_file_name.c_str()); 
 }
